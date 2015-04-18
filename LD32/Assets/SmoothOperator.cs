06e58046fa3b58247e2dd2bfb01aac1f495b9263
paul pettipas <paul.pettipas@gmail.com>
2015-04-18 12:00:40 -0300

    climb animation for science

---
+++ LD32/Assets/SmoothOperator.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SmoothOperator : MonoBehaviour {
+
+	public Transform ourHero;
+	Vector3 dampingVelocity;
+	void Update () {
+		if(ourHero == null){
+			return;
+		}
+		Vector3 targetPosition = ourHero.position;
+		transform.position = Vector3.SmoothDamp(transform.position, new Vector3(targetPosition.x, targetPosition.y, targetPosition.z-5), ref dampingVelocity,0.2f);
+	}
+}
--- LD32/Assets/states/ClimbUpDownState.cs
+++ LD32/Assets/states/ClimbUpDownState.cs
@@ -6,17 +6,24 @@ public class ClimbUpDownState : State {
 	public StateData data;
 	public float speed = 1;
 	[SerializeField]protected Transform body;
-	
+	public Animator bodyAnimation;
 
 	public void OnEnable()
 	{
-		body.transform.forward = new Vector3(0,0,0);
+		body.transform.forward = body.transform.parent.forward;
 		if(data == null){
 			data = GetComponent<StateData>();
 		}
 	}
 
 	void Update () {
+
+		if(data.Direction != Vector3.zero){
+			bodyAnimation.Play("climb");
+		}else{
+			bodyAnimation.Play("rest");
+		}
+
 		transform.Translate(new Vector3(0,data.Direction.z,0) * speed * Time.deltaTime);
 	}
 }
