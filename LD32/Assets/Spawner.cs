c52f53d1e82e033ccc879d84a97da59dc9aacfea
paul pettipas <paul.pettipas@gmail.com>
2015-04-19 11:54:03 -0300

    enemies explode, enemies can spawn

---
+++ LD32/Assets/Spawner.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+
+	public float duration;
+	public GameObject enemyPrefab;
+	public ParticleSystem spawnerSystem;
+	public int total;
+	public void Awake(){
+		spawnerSystem.enableEmission = false;
+		StartCoroutine(Spawn());
+	}
+
+	public IEnumerator Spawn(){
+		if(duration == 0){
+			duration = 60;
+		}
+		yield return new WaitForSeconds(duration);
+		total--;
+		spawnerSystem.enableEmission = true;
+		yield return new WaitForSeconds(2);
+		Instantiate(enemyPrefab,transform.position,transform.rotation);
+		yield return new WaitForSeconds(1);
+		spawnerSystem.enableEmission = false;
+		if(total > 0){
+			StartCoroutine(Spawn());
+		}
+	}
+
+}
--- LD32/Assets/states/Vitality.cs
+++ LD32/Assets/states/Vitality.cs
@@ -4,10 +4,15 @@ using System.Collections;
 public class Vitality : MonoBehaviour {
 
 	public int hits;
-
+	public GameObject death;
 	public void TakeHit(){
 		hits--;
 		GetComponent<MaterialFlasher>().FlashForTime(0.5f);
+
+		if(hits <=0){
+			Instantiate(death,transform.position,Quaternion.identity);
+			Destroy(gameObject);
+		}
 	}
 
 }
